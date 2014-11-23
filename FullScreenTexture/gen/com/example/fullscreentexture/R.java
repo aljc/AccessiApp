079d5bae6c745769b0a749944d0f626c49a76a23
Alice Chang <achang94@gmail.com>
2014-11-23 11:15:04 -0600

    added venmo/paypal integration demo

--- FullScreenTexture/gen/com/example/fullscreentexture/R.java
+++ FullScreenTexture/gen/com/example/fullscreentexture/R.java
@@ -25,8 +25,9 @@ public final class R {
         public static final int filter=0x7f020003;
         public static final int ic_launcher=0x7f020004;
         public static final int icons=0x7f020005;
-        public static final int overlays=0x7f020006;
-        public static final int sixlayers=0x7f020007;
+        public static final int mock5=0x7f020006;
+        public static final int overlays=0x7f020007;
+        public static final int sixlayers=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f090003;
